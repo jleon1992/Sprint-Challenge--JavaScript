d4cfafaf2bd65cd4a85f901844e9c86f4c931c92
jleon1992 <36936276+jleon1992@users.noreply.github.com>
2020-05-22 11:54:21 -0400

    array methods

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,6 +21,8 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(item => 
+  displayNames.push('item.animal_name', 'item.scientific_name'));
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -29,7 +31,11 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+// const lowCaseAnimalNames = [];
+  const lowCaseAnimalNames = zooAnimals.map(function(item){
+    return `${item.animal_name.toLowerCase()}, ${item.scientific_name.toLowerCase()}`;
+  });
+  
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -37,7 +43,16 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter(function(item){
+  return item.population < 5
+});
+        
+  // const smallCity = cityData.filter(function(item){
+  //   // true or false 
+  //   // true is included 
+  //   // false is excluded
+  //   return item.population < 500000;
+  // });
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -45,8 +60,13 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+const populationTotal =  zooAnimals.reduce(function(accumulator, item){
+    // console.log(`I am the accumulator ${accumulator}`);
+    // console.log(`I am the current value ${item.population}`);
+    return accumulator + item.population;
+  },0);
+console.log(populationTotal);;
+
 
 
 // ==== Callbacks ====  
